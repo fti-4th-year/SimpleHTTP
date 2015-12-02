6e5c47064b6d14e0e5e468d7847476c3b72f4818
kokoronomagnet <antonshreyner@gmail.com>
2015-12-03 00:45:25 +0600

    different requests

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <tcp/SocketListener.h>
 #include <sys/signal.h>
 #include <tcp/Socks4Manager.h>
+#include <iostream>
+#include <string>
+#include <regex>
 template< typename T , typename P >
 void operator-=( std::list< T > &out , P i )
 {
@@ -23,6 +26,19 @@ void sigpipeHndl( int sign )
 int main( int argc , char *argv[] )
 {
 	signal( SIGPIPE , sigpipeHndl ); //atoi( argv[ 1 ] )
+	/*std::string str = "GET /help HTTP/1.1";
+	 std::cout << str << "\n";
+	 std::regex re( "GET (.*)" );
+	 std::smatch match;
+	 //std::regex_match( str , sm , e );
+	 std::regex_search( str , match , re );
+	 for( auto i : match )
+	 {
+	 std::cout << std::regex_replace( str , std::regex( " |GET|HTTP/1.1|/" ) , "" );
+	 }
+	 exit( 0 );*/
+	ushort const port = 8080;
+	std::string const portstr = std::to_string( port );
 	SocketFactory factory( SocketListener( 8080 ) );
 	struct Pipe
 	{
@@ -37,25 +53,64 @@ int main( int argc , char *argv[] )
 	Waiter waiter;
 	int i = 0;
 	bool working = true;
-	waiter.pushSleepy( factory.getSocketListener( ) , [ &i , &waiter , &factory , &working ]()
-	{
-		try
-		{
-			SocketHandler in( factory.get( ) );
+	waiter.pushSleepy(
+			{ factory.getSocketListener( ) ,
+					[ portstr , &i , &waiter , &factory , &working ]()
+					{
+						try
+						{
+							SocketHandler in( factory.get( ) );
+							Msg request;
+							in >> request;
+							std::cout << request.getString();
+							std::string answer = "";
+							{
+								std::string str = request.getString();
+								std::regex re( "GET (.*)" );
+								std::smatch match;
+								if( std::regex_search( str , match , re ) )
+								{
+									std::string str = match[0];
+									std::string cmd = std::regex_replace( str , std::regex( " |GET|HTTP/1.1|/" ) , "" );
+									std::cout << cmd << "\n";
+									if( cmd == "help" )
+									{
+										answer = "there is no help.";
+									} else if( cmd == "info" )
+									{
+										answer = "parser is working.";
+									} else
+									{
+										answer = "error page";
+									}
+								}
+							}
+							if( answer != "" )
+							{
+								std::stringstream ss;
+								ss << "HTTP/1.1 200 OK\n";
+								//ss << "Content-length: 46\n";
+								ss << "Content-Type: text/html\n\n";
+								ss //<< "<html><head></head><body><div><h1>'"
+										<< answer;// << "'</h1></div></body></html>";
+								std::cout << ss.str() << "\n";
+								in << Msg( ss.str() );
+							} else
+							{
+								std::stringstream ss;
+								ss << "HTTP/1.1 200 OK\n";
+								//ss << "Content-length: 46\n";
+								ss << "Content-Type: text/html\n\n";
+								ss << "<html><head></head><body><div>simple page</div></body></html>";
+								in << Msg( ss.str() );
+							}
 
-			Msg request;
-			in >> request;
-			std::cout << request.getString() << "\n";
-			std::stringstream ss;
-			ss << "<head></head><body><br>hello number " << (i++);
-			ss << "</br></body>";
-			in << Msg( ss.str() );
-		} catch( std::exception const &e )
-		{
-			std::cerr << "main catcher:" << e.what() << "\n";
-			//working = false;
-		}
-	} );
+						} catch( std::exception const &e )
+						{
+							std::cerr << "main catcher:" << e.what() << "\n";
+							//working = false;
+						}
+					} , Waiter::READ } );
 	while( working )
 	{
 		try
